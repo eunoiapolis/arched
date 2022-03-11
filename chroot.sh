2b13e82d10423f30ba2b6de786302422a8a1f176
Aman Kushwaha <101411877+eunoiapolis@users.noreply.github.com>
2022-03-12 00:44:13 +0530

    line fix

--- chroot.sh
+++ chroot.sh
@@ -5,7 +5,7 @@ set -e
 cd ~
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
-sed -i '177s/.//' /etc/locale.gen
+sed -i '178s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=us" >> /etc/vconsole.conf
